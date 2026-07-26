1fadcff26f081ef530d9fbb4d27d726404bf08d6
Dirk <123376244+DerD86@users.noreply.github.com>
2026-07-26 12:12:48 +0200

    Fix tooltip event args namespace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
