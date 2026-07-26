482ea74fefdcba41c6aa819b6459769b809e7457
Dirk Schulte <dirkschulte86@mail.de>
2026-07-26 13:08:37 +0200

    v0.031

--- obj/Debug/net8.0-windows/SatoshiTicker.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/SatoshiTicker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Zeigt den Euro-Gegenwert eines manuell eingetragenen Bitcoin-Bestands und die Kur" +
     "sänderung der letzten zehn Minuten.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67535c23bda6ea572917ad75561518d2cd7bdbc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7adf39f43ae23c9c26d8be2389e288cce2fe070")]
 [assembly: System.Reflection.AssemblyProductAttribute("Satoshi Ticker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SatoshiTicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
